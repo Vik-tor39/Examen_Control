02484282dfced7edc7897cae871bc5842a07857f
Víctor Suquilanda <victor.suquilanda@udla.edu.ec>
2023-05-03 15:32:41 -0500

    Commit 3: Código controlado

--- Control.c
+++ Control.c
@@ -2,23 +2,28 @@
 
 int main(){
     int num,i;
-    printf("Ingrese la cantidad de elementos que desea obtener de la serie de Fibonacci:\n");
-    scanf("%d",&num);
     int sum=0;
     int Fib[100];
     Fib[0]=0;
     Fib[1]=1;
-    for (i=2;i<num;i++){
-        Fib[i]=Fib[i-1]+Fib[i-2];
+    printf("Ingrese la cantidad de elementos que desea obtener de la serie de Fibonacci:\n");
+    scanf("%d",&num);
+    while (num<=0){
+        printf("El numero ingresado es menor a cero, por favor ingrese un numero entero positivo:\n");
+        scanf("%d",&num);
     }
-    printf("La serie de Fibonnaci es:");
-    for (i=0;i<num;i++){
-        printf ("%d ", Fib[i]);
-    }
-    
-    for (i=0; i<num;i++){
-        sum=sum+Fib[i];
-    }
-    printf("\nLa sumtoria de la serie dada es: %d",sum);
+        
+        for (i=2;i<num;i++){
+            Fib[i]=Fib[i-1]+Fib[i-2];
+        }
+        printf("La serie de Fibonnaci es:");
+        for (i=0;i<num;i++){
+            printf ("%d ", Fib[i]);
+        }
+        for (i=0; i<num;i++){
+            sum=sum+Fib[i];
+        }
+        printf("\nLa sumtoria de la serie dada es: %d",sum);
+        
     return 0;
 }
